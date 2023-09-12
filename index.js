cf4b6d6c49967b93d82fc2017983e3a4a2f29540
Kenan Yildirim <kenan@kenany.me>
2023-09-12 09:16:37 -0400

    feat: drop Node.js v14 and v16
    BREAKING CHANGE: Node.js v14 and v16 are no longer supported.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var split = require('binary-split');
-var fs = require('graceful-fs');
-var once = require('once');
+const split = require('binary-split');
+const fs = require('graceful-fs');
+const once = require('once');
 
 /**
  * @param {fs.PathLike} file
@@ -9,14 +9,14 @@ var once = require('once');
 function firstLine(file, callback) {
   callback = once(callback);
 
-  var splitter = split();
+  const splitter = split();
   splitter.on('data', function(item) {
     callback(null, item);
     splitter.end();
   });
   splitter.on('error', callback);
 
-  var stream = fs.createReadStream(file);
+  const stream = fs.createReadStream(file);
 
   // catch ENOENT
   stream.on('error', callback);
